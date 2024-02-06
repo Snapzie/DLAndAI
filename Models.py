1225e844ff453e3680de071b3ab26013be6dccba
Snapzie <casper@bresdahl.dk>
2024-02-06 17:59:23 +0100

    Added encoder loop as seperated class and added global hyperparameter structure

--- Models.py
+++ Models.py
@@ -1,30 +1,52 @@
 import torch
 import torch.nn as nn
-import torch.functional as f
+import torch.nn.functional as f
 
-class InputEmbeddings(nn.Module):
-    def __init__(self,d_model,vocab_size):
-        super().__init__()
-        self.d_model = d_model
-        self.vocab_size = vocab_size
-        self.embedding = nn.Embedding(vocab_size,d_model)
+# Hyper parameters
+_N = 1
+_d_model = 512
+_seq_len = 128
+_num_heads = 8
+_dropout = 0
+_vocab_size = 28
+# Read input file
+# _vocab_size = len(set(input))
 
-class Encoder(nn.Module):
-    def __init__(self,N,d_model,seq_len,num_heads,dropout=0):
+class EncoderLoop(nn.Module):
+    def __init__(self,d_model,num_heads,seq_len):
         super().__init__()
-        assert(d_model % num_heads == 0,'d_model not divisionable by num_heads')
-        self.N = N
-        self.d_model = d_model
+        assert d_model % num_heads == 0,'d_model not divisionable by num_heads'
         self.seq_len = seq_len
-        self.num_heads = num_heads
-        self.dropout = dropout
-
-        self.MHA = nn.MultiheadAttention(self.d_model,self.num_heads,dropout=self.dropout,batch_first=True)
-        self.FF = nn.Linear(self.d_model,self.d_model)
-        self.norm1 = nn.BatchNorm1d(self.d_model)
+        self.MHA = nn.MultiheadAttention(d_model,num_heads,dropout=_dropout,batch_first=True)
+        self.lin1 = nn.Linear(d_model,d_model)
+        self.lin2 = nn.Linear(d_model,d_model)
+        self.norm1 = nn.LayerNorm(d_model)
+        self.norm2 = nn.LayerNorm(d_model)
+        self.dropout = nn.Dropout(_dropout)
 
     def forward(self,x):
-        # Attention(QW,KW,VW)
-        # x @ w: (seq,d_model) @ (d_model,d_model) --> (seq,d_model)
-        self.register_buffer('tril', torch.tril(torch.ones((self.seq_len,self.d_model),dtype=bool),diagonal=0))
+        # Attention(QW,KW,VW): x @ w <==> (seq,d_model) @ (d_model,d_model) --> (seq,d_model)
+        x_residual1 = x
+        self.register_buffer('tril', torch.tril(torch.ones((self.seq_len,self.seq_len),dtype=bool),diagonal=0))
         x,attn_scores = self.MHA.forward(x,x,x,attn_mask=self.tril)
+        x_residual2 = self.norm1(x + x_residual1)
+        x = self.dropout(f.relu(self.lin1(x_residual2)))
+        x = self.lin2(x)
+        x = self.norm2(x + x_residual2)
+
+class Encoder(nn.Module):
+    def __init__(self,d_model,num_heads,seq_len):
+        super().__init__()
+        self.TokentEmbedding = nn.Embedding(d_model,_vocab_size)
+        self.PosEmbedding = nn.Embedding(_seq_len,d_model) #TODO: Replace with cos- and sinusoidal embedding
+        self.loop = nn.Sequential(*[EncoderLoop(d_model,num_heads,seq_len) for _ in range(_N)])
+
+    def forward(self,x):
+        #token_embedding = self.TokentEmbedding(x)
+        #os_embedding = self.PosEmbedding(x)
+        # x = token_embedding + pos_embedding
+        x = self.loop(x)
+
+model = Encoder(_d_model,_num_heads,_seq_len)
+x = torch.randn((1,_seq_len,_d_model))
+model.forward(x)
