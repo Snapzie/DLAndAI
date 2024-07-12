c1798437672f7989f3a002b48affa94562c72bfa
Snapzie <casper@bresdahl.dk>
2024-07-12 12:33:56 +0200

    Added Discriminator

--- SRGAN/Train.py
+++ SRGAN/Train.py
@@ -2,7 +2,7 @@ import torch
 import torch.nn.functional as F
 import torch.nn as nn
 
-class Res_block(nn.Module):
+class Gen_res_block(nn.Module):
     def __init__(self):
         super().__init__()
         self.block = nn.Sequential(
@@ -17,7 +17,7 @@ class Res_block(nn.Module):
         x = self.block(x_in)
         return x_in + x
     
-class Conv_block(nn.Module):
+class Gen_conv_block(nn.Module):
     def __init__(self):
         super().__init__()
         self.block = nn.Sequential(
@@ -35,10 +35,10 @@ class Generator(nn.Module):
         super().__init__()
         self.conv_1 = nn.Conv2d(3,64,9,padding='same')
         self.prelu = nn.PReLU(64)
-        self.residual_blocks = nn.Sequential(*[Res_block() for _ in range(n_residual_blocks)])
+        self.residual_blocks = nn.Sequential(*[Gen_res_block() for _ in range(n_residual_blocks)])
         self.conv_2 = nn.Conv2d(64,64,3,padding='same')
         self.bn = nn.BatchNorm2d(64)
-        self.conv_blocks = nn.Sequential(*[Conv_block() for _ in range(n_conv_blocks)])
+        self.conv_blocks = nn.Sequential(*[Gen_conv_block() for _ in range(n_conv_blocks)])
         self.conv_3 = nn.Conv2d(64,3,9,padding='same')
     
     def forward(self,x):
@@ -51,6 +51,37 @@ class Generator(nn.Module):
         x = self.conv_blocks(x)
         x = self.conv_3(x)
         return x
+    
+class Disc_block(nn.Module):
+    def __init__(self,n,s):
+        super().__init__()
+        in_size = n if s == 2 else n//2
+        padding = 'same' if s == 1 else 1
+        self.conv = nn.Conv2d(in_size,n,3,stride=s,padding=padding)
+        self.bn = nn.BatchNorm2d(n)
+    
+    def forward(self,x):
+        x = self.conv(x)
+        x = self.bn(x)
+        return F.leaky_relu(x)
+
+class Discriminator(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.conv = nn.Conv2d(3,64,3,padding='same')
+        block_params = [(64,2),(128,1),(128,2),(256,1),(256,2),(512,1),(512,2)]
+        self.blocks = nn.Sequential(*[Disc_block(n,s) for n,s in block_params])
+        self.flatten = nn.Flatten()
+        self.dense_1 = nn.Linear(512*6*6,1024)
+        self.dense_2 = nn.Linear(1024,1)
+    
+    def forward(self,x):
+        x = F.leaky_relu(self.conv(x))
+        x = self.blocks(x)
+        x = self.flatten(x)
+        x = F.leaky_relu(self.dense_1(x))
+        x = self.dense_2(x)
+        return F.sigmoid(x)
 
 g_model = Generator()
 x = torch.rand((1,3,24,24))
@@ -58,3 +89,8 @@ print(f'G_in: {x.shape}')
 out = g_model(x)
 print(f'G_out: {out.shape}')
 
+disc_model = Discriminator()
+x = torch.randn((1,3,96,96))
+print(f'Disc_in: {x.shape}')
+out = disc_model(x)
+print(f'Disc_out: {out.shape}')
